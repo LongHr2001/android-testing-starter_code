235b30cb61dddc9a82801de69fe6d08c9daf7c9e
LongHr2001 <mitom2007@gmail.com>
2022-04-12 22:35:04 +0700

    Testing 2.0

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,26 +1,47 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
 import getOrAwaitValue
 import junit.framework.TestCase
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
-@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var tasksViewModel: TasksViewModel
+
+    private lateinit var tasksRepository: FakeTestRepository
+
+    @Before
+    fun setupViewModel() {
+        // We initialise the tasks to 3, with one active and two completed
+        tasksRepository = FakeTestRepository()
+        val task1 = Task("Title1", "Description1")
+        val task2 = Task("Title2", "Description2", true)
+        val task3 = Task("Title3", "Description3", true)
+        tasksRepository.addTasks(task1, task2, task3)
+
+        tasksViewModel = TasksViewModel(tasksRepository)
+
+    }
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
 
         // Given a fresh ViewModel
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        val tasksViewModel = TasksViewModel(tasksRepository)
 
         // When adding a new task
         tasksViewModel.addNewTask()
@@ -34,7 +55,7 @@ class TasksViewModelTest {
     fun setFilterAllTasks_tasksAddViewVisible() {
 
         // Given a fresh ViewModel
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        val tasksViewModel = TasksViewModel(tasksRepository)
 
         // When the filter type is ALL_TASKS
         tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
